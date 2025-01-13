6867a0758efbd6035c336905d43fc998081db7b5
Shawn Haman <18029943@saultcollege.ca>
2025-01-13 14:36:12 -0500

    lab 0

---
+++ src/test/java/CSD230/csd230/lecture_2_1/ShawnHaman/ApplicationTests.java
@@ -0,0 +1,13 @@
+package CSD230.csd230.lecture_2_1.ShawnHaman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
